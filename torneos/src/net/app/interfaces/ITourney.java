4192d656e66cccbd6d326d9990f02e2bfc208c2c
Abby <gp3123@gmail.com>
2023-08-04 22:09:50 -0300

    Added some comments

--- torneos/src/net/app/interfaces/ITourney.java
+++ torneos/src/net/app/interfaces/ITourney.java
@@ -21,8 +21,10 @@ public interface ITourney {
 
     List<IMatch> getMatches();
 
+    /* Prompt in console asking what players do you want to switch using the overloaded method */
     void switchPlayers();
 
+    /* Switch two player1 and player2 between their matches */
     void switchPlayers(IPlayer p1, IPlayer p2) throws PlayerIsOnAMatchException;
 
     void printScheduling();
